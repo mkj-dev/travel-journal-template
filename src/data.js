29f1ba0051cfcc7974ec4c5bd9ed9f63f4157e21
mstach0wicz <mikolaj.stachowicz0@gmail.com>
2022-11-01 23:41:56 +0100

    Add journey data objects

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const journeyData = [
+    {
+        id : 1,
+        photo: 'https://source.unsplash.com/WLxQvbMyfas',
+        country: 'Japan',
+        place: 'Mount Fouji',
+        visitDate: '12 Jan, 2021 - 24 Jan, 2021',
+        information: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae, ipsa ex culpa nesciunt, adipisci pariatur inventore accusantium facere quibusdam error cum modi illo, magni atque doloribus quidem aspernatur accusamus sit iusto reprehenderit tenetur alias doloremque? Consequatur, distinctio quisquam dignissimos sit dolores modi incidunt neque, praesentium doloribus dolorum nulla necessitatibus?'
+    },
+    {
+        id : 2,
+        photo: 'https://source.unsplash.com/JmuyB_LibRo',
+        country: 'Australia',
+        place: 'Sydney Opera House',
+        visitDate: '27 May, 2021 - 8 Jun, 2021',
+        information: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae, ipsa ex culpa nesciunt, adipisci pariatur inventore accusantium facere quibusdam error cum modi illo, magni atque doloribus quidem aspernatur accusamus sit iusto reprehenderit tenetur alias doloremque? Consequatur, distinctio quisquam dignissimos sit dolores modi incidunt neque, praesentium doloribus dolorum nulla necessitatibus?'
+    },
+    {
+        id : 3,
+        photo: 'https://source.unsplash.com/3PeSjpLVtLg',
+        country: 'Norway',
+        place: 'Geirangerfjord',
+        visitDate: '01 Oct, 2021 - 18 Nov, 2021',
+        information: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus recusandae, ipsa ex culpa nesciunt, adipisci pariatur inventore accusantium facere quibusdam error cum modi illo, magni atque doloribus quidem aspernatur accusamus sit iusto reprehenderit tenetur alias doloremque? Consequatur, distinctio quisquam dignissimos sit dolores modi incidunt neque, praesentium doloribus dolorum nulla necessitatibus?'
+    },
+]
+
+export default journeyData
